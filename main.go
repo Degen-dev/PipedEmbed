dd90a60a055f0f1555ae5a2e25c396a72b808dab
Degenerate <82691315+Degen-dev@users.noreply.github.com>
2023-03-13 09:57:04 -0500

    basic changes

--- main.go
+++ main.go
@@ -14,7 +14,6 @@ func main() {
 
 	type ApiResponse struct {
 		Title       string `json:"title"`
-		Description string `json:"description"`
 		ThumbNail   string `json:"thumbnailUrl"`
 		Uploader    string `json:"uploader"`
 		Duration    int    `json:"duration"`
@@ -23,33 +22,33 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html")
+		
 		if r.URL.Path == "/" {
 			_, err := fmt.Fprintf(w, "<!DOCTYPE html><head><meta http-equiv=\"Refresh\" content=\"0; url='https://piped.kavin.rocks/'\"></head>")
 			if err != nil {
-				return
+				log.Println("An error occured when trying to redirect user: " + err)
 			}
 		} else if r.URL.Path != "/" {
+			var embedInfo ApiResponse
 			path := "https://pipedapi.kavin.rocks/streams/" + r.URL.Path
 			resp, err := http.Get(path)
 			if err != nil {
-				log.Fatal(err)
+				log.Println("An error occured when trying to get video info: " + err)
 			}
 			defer func(Body io.ReadCloser) {
 				err := Body.Close()
 				if err != nil {
-					log.Fatal(err)
+					log.Fatal("FATAL ERROR OCCURED WHEN TRYING TO CLOSE REQUEST: " + err)
 				}
 			}(resp.Body)
 			body, err := ioutil.ReadAll(resp.Body)
-
-			var embedInfo ApiResponse
 			if err := json.Unmarshal(body, &embedInfo); err != nil {
-				log.Fatal(err)
+				log.Println("An error occured when trying to parse JSON response: " + err)
 			}
 
-			_, err = fmt.Fprintf(w, "<!DOCTYPE html><head><meta content='%s' property='og:title'><meta content='Channel: %s | Views: %d | Duration: %s' property='og:description'><meta content='https://piped.kavin.rocks%s' property='og:url'><meta content='%s' property='og:image'><meta http-equiv=\"Refresh\" content=\"0; url='https://piped.kavin.rocks%s'\"></head>", embedInfo.Title, embedInfo.Uploader, embedInfo.Views, sortTime(embedInfo.Duration), r.URL.Path, embedInfo.ThumbNail, r.URL.Path)
+			_, err = fmt.Fprintf(w, "<!DOCTYPE html><head><meta content=\"%s\" property=\"og:title\"><meta content=\"Channel: %s | Views: %d | Duration: %s\" property=\"og:description\"><meta content=\"https://piped.kavin.rocks%s\" property=\"og:url\"><meta content=\"%s\" property=\"og:image\"><meta http-equiv=\"Refresh\" content=\"0; url=\"https://piped.kavin.rocks%s\"\"></head>", embedInfo.Title, embedInfo.Uploader, embedInfo.Views, sortTime(embedInfo.Duration), r.URL.Path, embedInfo.ThumbNail, r.URL.Path)
 			if err != nil {
-				return
+				log.Println("An error occured when trying to send video data: " + err)
 			}
 		}
 	})
