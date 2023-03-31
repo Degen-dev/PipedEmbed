e41efca93650fb1043d497a57b76342ab078d3e4
Degen-dev <degenerate@tutamail.com>
2023-03-30 19:15:26 -0500

    switch from net/http to fasthttp

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/Degen-dev/PipedEmbed
 
 go 1.20
 
-require golang.org/x/text v0.8.0 // indirect
+require (
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/klauspost/compress v1.16.3 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.45.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,59 +3,50 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/valyala/fasthttp"
 	"golang.org/x/text/language"
 	"golang.org/x/text/message"
-	"io"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"strconv"
 )
 
-func main() {
+type ApiResponse struct {
+	Title     string `json:"title"`
+	ThumbNail string `json:"thumbnailUrl"`
+	Uploader  string `json:"uploader"`
+	Duration  int    `json:"duration"`
+	Views     int    `json:"views"`
+}
 
-	type ApiResponse struct {
-		Title     string `json:"title"`
-		ThumbNail string `json:"thumbnailUrl"`
-		Uploader  string `json:"uploader"`
-		Duration  int    `json:"duration"`
-		Views     int    `json:"views"`
+func main() {
+	requestHandler := func(ctx *fasthttp.RequestCtx) {
+		ctx.SetContentType("text/html")
+
+		if string(ctx.Path()) == "/" {
+			ctx.WriteString("<!DOCTYPE html><head><meta http-equiv=\"Refresh\" content=\"0; url='https://piped.kavin.rocks/'\"></head>")
+		} else if string(ctx.Path()) != "/" {
+			var embedInfo ApiResponse
+			path := "https://pipedapi.kavin.rocks/streams" + string(ctx.Path())
+			statusCode, body, err := fasthttp.Get(nil, path)
+			if err != nil || statusCode != fasthttp.StatusOK {
+				log.Println("An error occurred when trying to get video info:", err)
+				return
+			}
+
+			if err := json.Unmarshal(body, &embedInfo); err != nil {
+				log.Println("An error occurred when trying to parse JSON response:", err)
+				return
+			}
+
+			ctx.WriteString(fmt.Sprintf("<!DOCTYPE html><head><meta content=\"%s\" property=\"og:title\"><meta content=\"Channel: %s | Views: %s | Duration: %s\" property=\"og:description\"><meta content=\"https://piped.kavin.rocks%s\" property=\"og:url\"><meta content=\"%s\" property=\"og:image\"><meta http-equiv=\"Refresh\" content=\"0; url='https://piped.kavin.rocks%s'\"></head>", embedInfo.Title, embedInfo.Uploader, sortViews(embedInfo.Views), sortTime(embedInfo.Duration), string(ctx.Path()), embedInfo.ThumbNail, string(ctx.Path())))
+		}
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html")
+	server := fasthttp.Server{
+		Handler: requestHandler,
+	}
 
-		if r.URL.Path == "/" {
-			_, err := fmt.Fprintf(w, "<!DOCTYPE html><head><meta http-equiv=\"Refresh\" content=\"0; url='https://piped.kavin.rocks/'\"></head>")
-			if err != nil {
-				log.Println("An error occured when trying to redirect user: ", err)
-			}
-		} else if r.URL.Path != "/" {
-			var embedInfo ApiResponse
-			path := "https://pipedapi.kavin.rocks/streams/" + r.URL.Path
-			resp, err := http.Get(path)
-			if err != nil {
-				log.Println("An error occured when trying to get video info: ", err)
-			}
-			defer func(Body io.ReadCloser) {
-				err := Body.Close()
-				if err != nil {
-					log.Println("An error occured when trying to get JSON response ", err)
-				}
-			}(resp.Body)
-			body, err := ioutil.ReadAll(resp.Body)
-			if err := json.Unmarshal(body, &embedInfo); err != nil {
-				log.Println("An error occured when trying to parse JSON response: ", err)
-			}
-
-			_, err = fmt.Fprintf(w, "<!DOCTYPE html><head><meta content=\"%s\" property=\"og:title\"><meta content=\"Channel: %s | Views: %s | Duration: %s\" property=\"og:description\"><meta content=\"https://piped.kavin.rocks%s\" property=\"og:url\"><meta content=\"%s\" property=\"og:image\"><meta http-equiv=\"Refresh\" content=\"0; url='https://piped.kavin.rocks%s'\"></head>", embedInfo.Title, embedInfo.Uploader, sortViews(embedInfo.Views), sortTime(embedInfo.Duration), r.URL.Path, embedInfo.ThumbNail, r.URL.Path)
-			if err != nil {
-				log.Println("An error occured when trying to send video data: ", err)
-			}
-		}
-	})
-
-	log.Fatal(http.ListenAndServe(":9072", nil))
+	log.Fatal(server.ListenAndServe(":9072"))
 }
 
 func sortTime(num int) string {
